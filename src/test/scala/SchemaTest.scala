326b6e6cc976c7fca3c6a924ff0bd6d908e07db7
yennanliu <f339339@gmail.com>
2020-11-13 00:32:18 +0800

    update test : test/scala/SchemaTest.scala

--- src/test/scala/SchemaTest.scala
+++ src/test/scala/SchemaTest.scala
@@ -1,5 +1,7 @@
 import org.scalatest.flatspec.AnyFlatSpec
 
+// https://docs.scala-lang.org/tour/case-classes.html
+
 // UDF
 import schema.adhocSchema
 
@@ -15,5 +17,13 @@ class SchemaTest extends AnyFlatSpec{
     assert (msg.sender === "tom")
     assert (msg.recipient === "jim")
     assert (msg.body === "hello there")
+  }
+
+  "Message's values and it copy values" should "equal as expect" in {
+    val msg2 = adhocSchema.Message("julien@bretagne.fr", "travis@washington.us", "Me zo o komz gant ma amezeg")
+    val msg3 = msg2.copy(sender = msg2.recipient, recipient = "claire@bourgogne.fr")
+    assert (msg3.sender === "travis@washington.us")
+    assert (msg3.recipient === "claire@bourgogne.fr")
+    assert (msg3.body === "Me zo o komz gant ma amezeg")
   }
 }
