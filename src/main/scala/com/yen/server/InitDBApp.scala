356607f6f7a2dba1ae527d64d234bf152b7615bb
yennanliu <f339339@gmail.com>
2020-11-15 00:32:07 +0800

    fix db_init, db_delete endpoint (InitDBApp.scala), fix db location (application.conf

--- src/main/scala/com/yen/server/InitDBApp.scala
+++ src/main/scala/com/yen/server/InitDBApp.scala
@@ -1,9 +1,13 @@
 package com.yen.server
 
 // app init SQLite database
+// https://github.com/twitter/finatra/blob/develop/http/src/test/scala/com/twitter/finatra/http/tests/integration/doeverything/main/controllers/DoEverythingController.scala
+
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.routing.HttpRouter
 import com.twitter.finatra.http.{Controller, HttpServer}
+
+import sys.process._
 
 // UDF
 import services.SQLiteRunner.DBRunner
@@ -16,16 +20,26 @@ class DBServer extends HttpServer {
   override protected def configureHttp(router: HttpRouter) {
     router.add[HelloController]
     router.add[DBController]
+    router.add[DBDeleter]
   }
 }
 
-class HelloController extends  Controller {
+class HelloController extends Controller {
   get("/hello") {request: Request => "FitmanApp Hello world !"}
 }
 
-class DBController extends  Controller {
-  val db_runner = new DBRunner
-  val setupFuture = db_runner.db.run(db_runner.setup)
-  get("/db_init") {request: Request => "init SQLITE DB!"}
+class DBController extends Controller {
+  get("/db_init") { _:  Request =>
+    val db_runner = new DBRunner
+    val setupFuture = db_runner.db.run(db_runner.setup)
+    "init DB !"
+  }
 }
 
+class DBDeleter extends Controller {
+  get("/db_delete") { _:  Request =>
+    val deleteCommand = "rm db/coffees.db"
+    val output = deleteCommand.!!
+    "delete DB !"
+  }
+}
