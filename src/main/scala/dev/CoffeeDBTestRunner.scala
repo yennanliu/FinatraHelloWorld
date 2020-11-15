a852f9dca7389f4873013511b4f363a207bb65ee
yennanliu <f339339@gmail.com>
2020-11-15 13:18:49 +0800

    add UserSchema.scala, backup code

--- src/main/scala/dev/CoffeeDBTestRunner.scala
+++ src/main/scala/dev/CoffeeDBTestRunner.scala
@@ -3,7 +3,7 @@ package dev
 // UDF
 import utils.SQLUtils
 
-object SQLiteTestRunner extends App{
+object CoffeeDBTestRunner extends App{
   println("SQLiteTestRunner run ...")
   val sql_utils = new SQLUtils
   sql_utils.showDBData()
