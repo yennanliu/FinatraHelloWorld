80a251194555d9b8b1c39c9ed013b7b863f9a6ca
yennanliu <f339339@gmail.com>
2020-12-02 08:19:51 +0800

    update test/scala/MsgCheckerTest.scala

--- src/test/scala/MsgCheckerTest.scala
+++ src/test/scala/MsgCheckerTest.scala
@@ -5,6 +5,28 @@ class MsgCheckerTest extends AnyFlatSpec{
 
   val msg_checker = new MsgChecker
 
+  "httpStatus" should "equal as expect" in {
+    val httpmsg1 = "SUCCESS"
+    assert (msg_checker.CheckSuccess (httpmsg1) === true)
+
+    val httpmsg2 = "FAIL"
+    assert (msg_checker.CheckSuccess (httpmsg2) === false)
+
+    val httpmsg3 = "this is not validated httpmsg"
+    assert (msg_checker.CheckSuccess (httpmsg3) === false)
+  }
+
+  "httpStatusCode" should "equal as expect" in {
+    val httpCode1 = 201
+    assert (msg_checker.CheckHttpStatusCode (httpCode1) === true)
+
+    val httpCode2 = 404
+    assert (msg_checker.CheckHttpStatusCode (httpCode2) === false)
+
+    val httpmsg3 = "this is not validated httpmsg"
+    assert (msg_checker.CheckSuccess (httpmsg3) === false)
+  }
+
   "List1 length" should "equal as expect" in {
     val myList_1 = List(1,2,3)
     assert (msg_checker.CheckListLength (myList_1) === "equals or larger than 3")
